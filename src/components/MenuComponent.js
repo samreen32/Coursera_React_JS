8ea1c6348e304f7bc9da3a194722b73a4a68452e
Samreen <iamsamreenk@gmail.com>
2022-07-18 18:39:30 +0500

    2nd Week-FirstCommit-Presentational and Container Components

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,43 +1,17 @@
 import React, {Component} from "react";
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
-import Dishdetail from "./DishdetailComponent";
 
 class Menu extends Component{
 
   constructor(props) {
     super(props);
-    this.state = {
-      selectedDish: null
-    };
-
   }
- 
-  onDishSelect(dish) {
-    this.setState({ selectedDish: dish});
-  }
-
-  /*renderDish(dish) {
-    if (dish != null)
-        return(
-            <Card>
-                <CardImg top src={dish.image} alt={dish.name} />
-                <CardBody>
-                  <CardTitle>{dish.name}</CardTitle>
-                  <CardText>{dish.description}</CardText>
-                </CardBody>
-            </Card>
-        );
-    else
-        return(
-            <div></div>
-        );
-  }*/
 
   render() {
     const menu = this.props.dishes.map((dish) => {
         return (
-          <div key={dish.id} className="col-12 col-md-5 m-1">   {/* col-12.... use for larger screen sizes occupy column one below the other. col-md-5... medium to small and extra small screen sizes */}
-            <Card key={dish.id} onClick={() => this.onDishSelect(dish)}>
+          <div key={dish.id} className="col-12 col-md-5 m-1">   {/* col-12.... use for larger screen sizes occupy column one below the other. col-md-5... medium to small and extra small screen sizes.*/}
+            <Card key={dish.id} onClick={() => this.props.onClick(dish.id)}>     {/* onClick function from MainComponent.js which is passing 'dish.id' as parameter.*/}
               <CardImg width="100%" src={dish.image} alt={dish.name} />
            
               <CardImgOverlay>
@@ -53,8 +27,8 @@ class Menu extends Component{
         <div className="row">
           {menu}
         </div>
-        <Dishdetail dish={this.state.selectedDish}/>
-        {/*<div className="row">
+     {/*<Dishdetail dish={this.state.selectedDish}/>
+        <div className="row">
           <div  className="col-12 col-md-5 m-1">
             {this.renderDish(this.state.selectedDish)}
           </div>
